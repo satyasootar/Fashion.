d6df379145d8b4e97bb38a3a609563c2f21c8460
satyasootar <satya.sootar06@gmail.com>
2025-03-22 21:22:09 +0530

    add initial React components and Tailwind CSS setup Navbar created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+
+
+function App() {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/placeOrder" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
