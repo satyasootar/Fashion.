1f4ff6850e04764065f1113ac82842945db78e38
satyasootar <satya.sootar06@gmail.com>
2025-03-23 20:45:15 +0530

    add LatestCollection and ProductItem components; update Navbar and CSS for responsiveness

--- frontend/src/components/LastestCollection.jsx
+++ frontend/src/components/LastestCollection.jsx
@@ -1,11 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+import ProductItem from './ProductItem'
 
 const LastestCollection = () => {
     const { products } = useContext(ShopContext)
-    console.log("products: ", products);
+    const [latestProduct, setLatestProduct] = useState([])
+
+    useEffect(() => {
+        setLatestProduct(products.slice(0, 10))
+    }, [])
+
     return (
-        <div>LastestCollection</div>
+        <div className='my-10' >
+            <div className='text-center py-8 text-3xl' >
+                <Title text1={"LATEST"} text2={" COLLECTIONS"} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600' >
+                    Dummy text
+                </p>
+            </div>
+            {/* Rendering Products */}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6' >
+                {
+                    latestProduct.map((item, index) => (
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
 
     return (
         <div className='flex items-center justify-between py-5 font-medium' >
+            <Link to='/'>
             <img src={assets.logo} className='w-36' />
+            </Link>
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink to='/' className='flex flex-col items-center gap-1' >
@@ -55,7 +57,7 @@ const Navbar = () => {
             </div>
 
             {/* side menue for smaller screens */}
-            
+
             <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`} >
                 <div className='flex flex-col text-gray-600' >
                     <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer' >
