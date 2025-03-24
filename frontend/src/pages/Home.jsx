faf598a4d063af9e27fc110df055cf306f4e0430
satyasootar <satya.sootar06@gmail.com>
2025-03-24 18:33:52 +0530

    add BestSeller component to display top-selling products; update Home page title and layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LastestCollection from '../components/LastestCollection'
+import { BestSeller } from '../components/BestSeller'
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <LastestCollection />
+            <BestSeller />
         </div>
     )
 }
